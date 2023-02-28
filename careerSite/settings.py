2c326efdb20012512fa134366a74ac8dfb5457d9
Lemson Mureya <lmureya@conncoll.edu>
2023-02-28 15:48:32 -0500

    added gitignore

--- careerSite/settings.py
+++ careerSite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+z@=91@lh$weub6xp0vchsam=y5mtz0emz*+^rd4_lfayrb-7('
+secret_key = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
